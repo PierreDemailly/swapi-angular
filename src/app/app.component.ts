8de05b075ec28bb2b77b7fde4badf962b1544866
Pierre Demailly <pierredemailly.pro@gmail.com>
2018-11-14 15:22:57 +0100

    create angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { StarwarsDataService } from './starwars-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'starwars';
+export class AppComponent implements OnInit {
+  title = 'Customer Management';
+  people: any[];
+
+  constructor(private dataService: StarwarsDataService) { }
+
+  ngOnInit() {
+    this.dataService.getCharacters()
+        .subscribe(data => this.people = data);
+}
 }
